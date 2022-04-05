1e49c80060846733e842e8b476dfde4d7513ca36
Olayeni Anifowose <olayeni45@gmail.com>
2022-04-05 13:34:51 +0000

    ref: Header and Home page

--- app/src/assets.js
+++ app/src/assets.js
@@ -1,5 +1,5 @@
 export const logoUrl =
-  "https://res.cloudinary.com/cloudev/image/upload/v1648933506/MoviesProject/Group_18_mduvtm.png";
+  "https://res.cloudinary.com/cloudev/image/upload/v1648933506/MoviesProject/Ranker.io.png";
 
 export const footerSvg =
   "https://res.cloudinary.com/cloudev/image/upload/v1648931767/MoviesProject/Ellipse_4_czdryx.svg";
--- app/src/components/shared/Header.jsx
+++ app/src/components/shared/Header.jsx
@@ -1,15 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import Logo from "../UI/Logo";
 import Back from "../UI/Back";
 import styles from "./css/Header.module.css";
 
 const Header = () => {
+  const navigate = useNavigate();
 
   return (
     <div className={styles.container}>
       <div className={styles.header}>
-        <div className={styles.logo}>
-            <Logo />
+        <div className={styles.logo} onClick={() => navigate("/")}>
+          <Logo />
         </div>
         <div className={styles.navigation}>
           <Back />
